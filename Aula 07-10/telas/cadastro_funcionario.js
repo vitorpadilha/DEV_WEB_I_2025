0dbbafe88465aca04d36e43dcfe3dc68f986696c
Your Name <you@example.com>
2025-07-11 18:08:27 -0300

    Aula 11 de Julho

--- Aula 07-10/telas/cadastro_funcionario.js
+++ Aula 07-10/telas/cadastro_funcionario.js
@@ -9,15 +9,27 @@ document.addEventListener("DOMContentLoaded",
             validaFormulario(campoNome.value, campoSalario.value, campoTelefone.value)?formCad.submit():null;
         });
         campoSalario.addEventListener("keyup", (ev2)=>{
-            
-        if(!["1","2","3","4","5","6","7","8","9","0",","].find((el)=>{
-            return ev2.key == el
-        })) {
-                campoSalario.value = campoSalario.value.substring(0, campoSalario.value.length-1);
-            }
+            validaSalario(campoSalario, ev2.key);
         });
     }
 );
 let validaFormulario = (nome, salario, telefone) => {
     return true;
+};
+
+let validaSalario = (campoSalario, charDigitado) => {
+    if(!["1","2","3","4","5","6","7","8","9","0",","].find((el)=>{
+            return charDigitado == el
+        })) 
+    {
+
+        //SE NAO FOR NUMERO OU VIRGULA, RETIRA O ÚLTIMO CARACTER DIGITADO
+                campoSalario.value = campoSalario.value.substring(0, campoSalario.value.length-2);
+        
+       
+    }
+    //SE A VÍRGULA JÁ FOI DIGITADA ANTES, TAMBÉM TIRA
+    if(charDigitado=="," && campoSalario.value.indexOf(",")<campoSalario.value.length-1) {
+        campoSalario.value = campoSalario.value.substring(0, campoSalario.value.length-1);
+    }
 };
