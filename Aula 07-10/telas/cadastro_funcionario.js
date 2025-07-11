7304fea9e16a3a5bf7afaaf2b40a36f6ace6df1c
Your Name <you@example.com>
2025-07-11 17:51:15 -0300

    Aula 11 de Julho

--- Aula 07-10/telas/cadastro_funcionario.js
+++ Aula 07-10/telas/cadastro_funcionario.js
@@ -8,12 +8,13 @@ document.addEventListener("DOMContentLoaded",
             let campoTelefone = document.getElementById("telefone");
             validaFormulario(campoNome.value, campoSalario.value, campoTelefone.value)?formCad.submit():null;
         });
-        campoSalario.addEventListener("keypress", (ev2)=>{
-          if(![1,2,3,4,5,6,7,8,9,0,","].find((el)=>{
-            ev2.key == el}))
-            {
-            campoSalario.value = campoSalario.value.substring(0, campoSalario.value.length-2);
-          }
+        campoSalario.addEventListener("keyup", (ev2)=>{
+            
+        if(!["1","2","3","4","5","6","7","8","9","0",","].find((el)=>{
+            return ev2.key == el
+        })) {
+                campoSalario.value = campoSalario.value.substring(0, campoSalario.value.length-1);
+            }
         });
     }
 );
