023948f4747a8bbb6c259ef5dfb3b1ba726a9df8
Your Name <you@example.com>
2025-07-24 14:23:19 -0300

    Aula 24/07/2025

--- Aula 07-10/service/funcionario.service.php
+++ Aula 07-10/service/funcionario.service.php
@@ -15,7 +15,7 @@
     }
 
     function listarFuncionario($filtroNome) {
-        $funcionarios = Funcionario::listar();
+        $funcionarios = Funcionario::listar($filtroNome);
         echo "<table><thead><tr><th>Nome</th><th>Salário</th><th>Telefone</th></tr></thead><tbody>";
         foreach($funcionarios as $funcionario) {
             echo "<tr><td>".$funcionario->nome."</td>";
