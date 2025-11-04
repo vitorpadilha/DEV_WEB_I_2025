5cb8d41ad8f1f36bd82eea75e5ef6b6a40ec462c
Vitor Padilha <vitorpadilhagoncalves@gmail.com>
2025-11-04 16:20:07 -0300

    AUla 04/11

--- Aula 04-11/controller/LivroController.php
+++ Aula 04-11/controller/LivroController.php
@@ -4,16 +4,46 @@ require_once __DIR__ . "/../model/livro.class.php";
 class LivroController implements GenericController{
     
     function cadastrar($dadosRecebidos){
-        
+        $livro = new Livro(
+            null,
+            $dadosRecebidos->titulo,
+            $dadosRecebidos->autor,
+            $dadosRecebidos->editora,
+            $dadosRecebidos->anoPublicacao,
+            $dadosRecebidos->genero,
+            $dadosRecebidos->localizacao,
+            $dadosRecebidos->ISSN
+        );
+        $livro->cadastrar();
     }
     function listar($dadosRecebidos){
         return Livro::listar($dadosRecebidos);
     }
     function alterar($dadosRecebidos){
-        
+        $livro = new Livro(
+            $dadosRecebidos->id,
+            $dadosRecebidos->titulo,
+            $dadosRecebidos->autor,
+            $dadosRecebidos->editora,
+            $dadosRecebidos->anoPublicacao,
+            $dadosRecebidos->genero,
+            $dadosRecebidos->localizacao,
+            $dadosRecebidos->ISSN
+        );
+        $livro->alterar();
     }
     function remover($dadosRecebidos){
-        
+        $livro = new Livro(
+            $dadosRecebidos->id,
+            $dadosRecebidos->titulo,
+            $dadosRecebidos->autor,
+            $dadosRecebidos->editora,
+            $dadosRecebidos->anoPublicacao,
+            $dadosRecebidos->genero,
+            $dadosRecebidos->localizacao,
+            $dadosRecebidos->ISSN
+        );
+        $livro->remover();
     }
 }
 
